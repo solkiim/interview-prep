1362238ac6c0d70d0a7e85995863793441fdec60
Sol Kim <solkim813@gmail.com>
2016-10-10 18:34:36 -0400

    ctci 1.5

--- ctci.py
+++ ctci.py
@@ -16,12 +16,14 @@ def stringUniqueChars2(s):
 			return False
 	return True
 	
+#----------------------------------------------------------------------
 #1.2
 def isPermutation(s1, s2):
 	s1 = ''.join(sorted(s1))
 	s2 = ''.join(sorted(s2))
 	return s1 == s2
 	
+#----------------------------------------------------------------------
 #1.3
 def URLify(s):
 	toReturn = ""
@@ -33,6 +35,7 @@ def URLify(s):
 	toReturn += arr[len(arr) - 1]
 	return toReturn
 
+#----------------------------------------------------------------------
 #1.4
 def palindromePermutation(s):
 	s = s.lower().strip().replace(" ", "")
@@ -51,3 +54,46 @@ def palindromePermutation(s):
 	if len(s) % 2 == 1:
 		return (numOdd == 1)
 	return True
+
+#----------------------------------------------------------------------
+#1.5
+def oneAway(s1, s2):
+	if len(s1) == len(s2):
+		return checkReplacement(s1, s2)
+	elif abs(len(s1) - len(s2)) == 1:
+		return checkInsertion(s1, s2)
+	else:
+		return False
+		
+def checkReplacement(s1, s2):
+	countDiff = 0
+	for i in range(len(s1)):
+		if s1[i] != s2[i]:
+			countDiff += 1
+	return (countDiff <= 1)
+	
+def checkInsertion(s1, s2):
+	shorter = ""
+	longer = ""
+	if len(s1) < len(s2):
+		shorter = s1
+		longer = s2
+	else:
+		shorter = s2
+		longer = s1
+	iShort = 0
+	iLong = 0
+	wasDiff = False
+	while iShort < len(shorter):
+		if longer[iLong] != shorter[iShort]:
+			if not wasDiff:
+				wasDiff = True
+				iLong += 1
+			else:
+				return False
+		else:
+			iLong += 1
+			iShort += 1
+	return True
+
+#----------------------------------------------------------------------
--- practice.py
+++ practice.py
@@ -1,3 +1,41 @@
-
+#1.5
+def oneAway(s1, s2):
+	if len(s1) == len(s2):
+		return checkReplacement(s1, s2)
+	elif abs(len(s1) - len(s2)) == 1:
+		return checkInsertion(s1, s2)
+	else:
+		return False
+		
+def checkReplacement(s1, s2):
+	countDiff = 0
+	for i in range(len(s1)):
+		if s1[i] != s2[i]:
+			countDiff += 1
+	return (countDiff <= 1)
 	
-print palindromePermutation("Tactost Coa")
+def checkInsertion(s1, s2):
+	shorter = ""
+	longer = ""
+	if len(s1) < len(s2):
+		shorter = s1
+		longer = s2
+	else:
+		shorter = s2
+		longer = s1
+	iShort = 0
+	iLong = 0
+	wasDiff = False
+	while iShort < len(shorter):
+		if longer[iLong] != shorter[iShort]:
+			if not wasDiff:
+				wasDiff = True
+				iLong += 1
+			else:
+				return False
+		else:
+			iLong += 1
+			iShort += 1
+	return True
+
+print oneAway("pale", "pales")
