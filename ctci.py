7def1ec701d908014c584f9b479d9fd037836ed6
Sol Kim <solkim813@gmail.com>
2016-10-10 17:49:42 -0400

    ctci 1.4

--- ctci.py
+++ ctci.py
@@ -32,3 +32,22 @@ def URLify(s):
 		toReturn += (arr[i] + "%20")
 	toReturn += arr[len(arr) - 1]
 	return toReturn
+
+#1.4
+def palindromePermutation(s):
+	s = s.lower().strip().replace(" ", "")
+	charsUsed = {}
+	for i in range(len(s)):
+		if s[i] in charsUsed:
+			charsUsed[s[i]] += 1
+		else:
+			charsUsed[s[i]] = 1
+	numOdd = 0;
+	for key, val in charsUsed.items():
+		if val % 2 == 1:
+			numOdd += 1
+	if numOdd > 1:
+		return False
+	if len(s) % 2 == 1:
+		return (numOdd == 1)
+	return True
